1c3c812c0bd5a7d04782731ba7be367a87d956d1
ruchipragda <pragdaruchi@gmail.com>
2025-03-20 11:05:17 +0530

    exam

---
+++ c++/exam/one.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {  
+    // char ab='a';
+    // int alphas = 3;
+    // do
+    // {
+    //     cout << ab << endl;
+    //     ab = ab + alphas;
+    // } while (ab<='z');    
+    char ab = 'a';
+    for (char i='a'; i<='z'; i+=4)
+    {
+        cout << i << endl;
+    }
+    
+}
+
---
+++ c++/exam/two.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main() {
+//     int n=123;
+//       int count=0;
+//       do{
+//           n=n/10;
+//         count++;
+//       }while (n);
+//       {
+//         cout << count;
+// }
+// int  count = 0;
+// int n;
+// cout << "Enter Any Number";
+// cin >> n;
+// while (n)
+// {
+//   n=n/10;
+//   count++;
+// }
+// cout << count;
+// }
+
+int count = 0;
+int r =1243;
+while (r)
+{
+   count++;
+   r = r /10;
+}
+cout  <<  count;
+}
+
